a92d53111b04165ca1311eaa72be73f7ddf49da0
allisonbusse <allisonbusse@gmail.com>
2019-08-21 10:39:27 -0700

    weater api

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 
 const mapsApi = require('./lib/maps-api');
-// const weatherApi = require('./lib/weather-api');
+const weatherApi = require('./lib/weather-api');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -24,36 +24,19 @@ app.get('/location', (request, response) => {
 });
 
 app.get('/weather', (request, response) => {
-    try {
-        const weather = request.query.weather;
-        const result = getWeather(weather);
-        response.status(200).json(result);
-    }
-    catch(err){
-        response.status(500).send('Sorry something went wrong, please try again', request.query);
-    }
+    const latitude = request.query.latitude;
+    const longitude = request.query.longitude;
+    
+    weatherApi.getForecast(latitude, longitude)
+        .then(forecast => {
+            response.json(forecast);
+        })
+        .catch(err => {
+            response.status(500).json({
+                error: err.message || err
+            });
+        });
 });
-
-const weatherData = require('./data/darksky.json');
-
-
-function getWeather(){
-    return toWeather(weatherData);
-}
-
-
-
-function toWeather() {
-    const daily = weatherData.daily;
-    const data = daily.data;
-    const dataResults = data.map((value) => {
-        return {
-            time: new Date(value.time * 1000).toISOString(),
-            forecast: value.summary
-        };
-    });
-    return dataResults;
-}
 
 app.listen(PORT, () => {
     console.log('server running on port', PORT);
